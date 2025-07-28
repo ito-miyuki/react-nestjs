0f6283c0c55404b6a27078cfa442ccc6a9a07477
ito-miyuki <mitou.1221@gmail.com>
2025-07-28 20:37:48 +0300

    fixed preact to React

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,43 +1,39 @@
-import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react'
 import './app.css'
 
-export function App() {
-  const [count, setCount] = useState(0)
+function App() {
+  const [todo, setTodo] = useState("");
+  const [list, setList] = useState<string[]>([]);
+
+  const handleAdd = (e: Event) => {
+    e.preventDefault();
+    if (!todo) return;
+    setList([...list, todo]);
+    setTodo("");
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
+      <h1>What to do today?</h1>
+      <form onSubmit={handleAdd}>
+        <label htmlFor="todo-input">Add to-do:</label>
+        <input
+          id="todo-input"
+          type="text"
+          value={todo}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setTodo(e.target.value)}
+        />
+        <button type="submit">Add</button>
+      </form>
+      <div className="list-container">
+        <ul>
+          {list.map((item, i) => (
+            <li key={i}>{item}</li>
+          ))}
+        </ul>
       </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p>
-        Check out{' '}
-        <a
-          href="https://preactjs.com/guide/v10/getting-started#create-a-vite-powered-preact-app"
-          target="_blank"
-        >
-          create-preact
-        </a>
-        , the official Preact + Vite starter
-      </p>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
     </>
   )
 }
+
+export default App;
